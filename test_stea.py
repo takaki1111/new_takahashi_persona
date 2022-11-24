a2b13a8b1a06348bc16502ce9aaf03f4e50d4f85
blm-takahashi <106038848+takaki1111@users.noreply.github.com>
2022-11-24 15:45:32 +0900

    Update test_stea.py

--- test_stea.py
+++ test_stea.py
@@ -52,7 +52,7 @@ def text_summary(prompt):
     temperature=0.8,
     max_tokens=100,
     top_p=1.0,
-    frequency_penalty=0.8,
+    frequency_penalty=2,
     presence_penalty=0.8,
     stop=["あなた:", "高橋:"]
     )
